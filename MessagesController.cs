05c7f5672aac385c03e04b9d4db26586a5cfe749
rachelsimoes <37052090+rachelsimoes@users.noreply.github.com>
2018-12-06 13:02:09 -0200

    Update MessagesController.cs

--- MessagesController.cs
+++ MessagesController.cs
@@ -10,10 +10,7 @@ namespace Pizzahelloworld
     [BotAuthentication]
     public class MessagesController : ApiController
     {
-        /// <summary>
-        /// POST: api/Messages
-        /// Receive a message from a user and reply to it
-        /// </summary>
+
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
             if (activity.Type == ActivityTypes.Message)
@@ -32,23 +29,18 @@ namespace Pizzahelloworld
         {
             if (message.Type == ActivityTypes.DeleteUserData)
             {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
             }
             else if (message.Type == ActivityTypes.ConversationUpdate)
             {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
+     
             }
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
+            
             }
             else if (message.Type == ActivityTypes.Typing)
             {
-                // Handle knowing tha the user is typing
+              
             }
             else if (message.Type == ActivityTypes.Ping)
             {
